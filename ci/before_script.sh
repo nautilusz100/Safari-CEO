2ecbc11afa0922423cc1c29d6c68681d0eed4b92
Weisz Márton <ctl3ye@inf.elte.hu>
2025-04-25 19:46:14 +0000

    Edit before_script.sh

--- ci/before_script.sh
+++ ci/before_script.sh
@@ -6,6 +6,13 @@ mkdir -p /root/.cache/unity3d
 mkdir -p /root/.local/share/unity3d/Unity/
 set +x
 
+echo "Checking UNITY_LICENSE format..."
+echo "$UNITY_LICENSE" | head -n 1
+if ! echo "$UNITY_LICENSE" | grep -q "<?xml"; then
+  echo "Invalid UNITY_LICENSE: Does not start with XML declaration"
+  exit 1
+fi
+
 UPPERCASE_BUILD_TARGET=${BUILD_TARGET^^};
 
 if [ $UPPERCASE_BUILD_TARGET = "ANDROID" ]
