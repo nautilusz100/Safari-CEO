80c8e250cdb9b70d5ac10c3d2c292189bde1c51c
Fischer Balázs <yg67qx@inf.elte.hu>
2025-04-30 09:25:19 +0000

    érdekes

--- ci/build.sh
+++ ci/build.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 echo "Building for $BUILD_TARGET"
 
-export BUILD_PATH=$UNITY_DIR/Builds/$BUILD_TARGET/
+export BUILD_PATH=$UNITY_DIR/Builds/$BUILD_TARGET/$BUILD_NAME/
 mkdir -p $BUILD_PATH
 
 ${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' unity-editor} \
